6281e722d2d0d4ea453cc954f57f2f7a5dbd616e
DESKTOP-J8DKHGB\dudwo <dudwo7783@naver.com>
2017-05-09 19:54:29 +0900

    for git test

--- cifar10_eval.py
+++ cifar10_eval.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-
+# git test
 """Evaluation for CIFAR-10.
 
 Accuracy:

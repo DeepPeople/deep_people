2ce9d765b11353307fe83d76539c2257c320cea0
DESKTOP-J8DKHGB\dudwo <dudwo7783@naver.com>
2017-05-16 19:36:29 +0900

    for gittest1

--- cifar10_eval.py
+++ cifar10_eval.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-#TESTTESTTESTTESTSETETESETET
+#TESTTESTTESTTESTSETETESETETasdasdasdasdasdsadasdasdsad
 """Evaluation for CIFAR-10.
 
 Accuracy:
